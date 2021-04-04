055718cc616e2ab1d362ce1a019aae8d720a13af
unknown <mlimaymanta@gmail.com>
2021-04-03 22:15:43 -0500

    Deploy to Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LineasDeNegocioComponent } from './components/lineas-de-negocio/lineas-de-negocio.component';
+import { MisionComponent } from './components/mision/mision.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LineasDeNegocioComponent,
+    MisionComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lineas-de-negocio/lineas-de-negocio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lineas-de-negocio',
+  templateUrl: './lineas-de-negocio.component.html',
+  styleUrls: ['./lineas-de-negocio.component.css']
+})
+export class LineasDeNegocioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
